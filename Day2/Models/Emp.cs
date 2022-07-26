ec6fbea5787dce464a895b06b81061123e40b229
AdarshVBhat <68853911+AdarshVBhat@users.noreply.github.com>
2022-07-26 17:32:02 +0530

    Add files via upload

---
+++ Day2/Models/Emp.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeCRUD.Models
+{
+    public class Emp
+    {
+        public int Empid { get; set; }
+        public string Ename { get; set; }
+        public string Job { get; set; }
+
+        public int Salary { get; set; }
+
+        public int Deptno { get; set; }
+    }
+}
