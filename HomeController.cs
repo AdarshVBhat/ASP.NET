4bc24785bc804884c08cdba25d0cfe7fbe140fba
AdarshVBhat <68853911+AdarshVBhat@users.noreply.github.com>
2022-07-26 15:56:29 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Employee()
+        {
+            return View();
+        }
+        
+
+        
+    }
+}
