63725f4b6d07c3558e27be8940f01be802b2f11f
AdarshVBhat <68853911+AdarshVBhat@users.noreply.github.com>
2022-07-28 22:07:27 +0530

    Add files via upload

---
+++ Day3/Repositories/IEmployeeRepo.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication03.Models;
+namespace WebApplication03.Repositories
+{
+    public interface IEmployeeRepo
+    {
+        List<Employee> GetAllEmployees();
+        Employee GetEmployeeById(int id);
+        void AddEmployee(Employee obj);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(Employee obj);
+    }
+}
